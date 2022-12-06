0a759e1a1da41cf9be588246454f888eebe75011
Alex Tung <tung_896404@tamu.edu>
2022-12-05 22:44:47 -0600

    fix?

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname + '/public'));
+app.set('views', './views');
 app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
